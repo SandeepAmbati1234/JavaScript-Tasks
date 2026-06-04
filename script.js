34d3636b0cb5cd41da27dc43fd5af14997714a95
Sandeep Ambati <sandeepambati541@gmail.com>
2026-06-04 18:16:09 +0530

    Added JavaScript tasks

---
+++ script.js
@@ -0,0 +1,91 @@
+// Task 1: Variable Practice
+
+let studentName = "Sandeep";
+let studentAge = 22;
+let studentCourse = "JavaScript";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+
+// Task 2: Var, Let, Const
+
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+company = "Google";
+salary = 30000;
+
+console.log(company);
+console.log(salary);
+console.log(country);
+
+
+// Task 3: User Name
+
+let userName = prompt("What is your name?");
+console.log("Welcome " + userName);
+
+
+// Task 4: Confirm Box
+
+let knowJS = confirm("Do you know JavaScript?");
+console.log(knowJS);
+
+
+// Task 5: Student Information
+
+let name = prompt("Enter Student Name:");
+let age = prompt("Enter Student Age:");
+let city = prompt("Enter Student City:");
+
+console.log("Name : " + name);
+console.log("Age : " + age);
+console.log("City : " + city);
+
+
+// Task 6: Alert Message
+
+let course = "JavaScript";
+alert("Welcome to " + course);
+
+
+// Task 7: Document Write
+
+document.writeln("Hello Student");
+document.writeln("<br>");
+document.writeln("Welcome to JavaScript");
+
+
+// Task 8: Favorite Things
+
+let favoriteFood = "Chicken Biryani";
+let favoriteColor = "Black";
+let favoriteMovie = "Leo";
+
+console.log("Food : " + favoriteFood);
+console.log("Color : " + favoriteColor);
+console.log("Movie : " + favoriteMovie);
+
+
+// Task 9: Mini Bio Data
+
+let myName = "Sandeep";
+let myAge = 22;
+let phone = "7036851274";
+let email = "sandeep@example.com";
+
+console.log("Name : " + myName);
+console.log("Age : " + myAge);
+console.log("Phone : " + phone);
+console.log("Email : " + email);
+
+
+// Task 10: Challenge
+
+let userName2 = prompt("Enter Your Name:");
+let userCourse = prompt("Enter Your Course:");
+
+alert("Hello " + userName2 + ", Welcome to " + userCourse + " Course");
